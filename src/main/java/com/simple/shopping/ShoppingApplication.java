68e39777ad92b7f81bd2bdaebe6e503f0ac8c7e4
lalwr <lalwrj@gmail.com>
2018-04-23 03:08:23 +0900

    feat : 프로젝트 기본 셋팅 및 회원, ROLE 테이블 작업

---
+++ src/main/java/com/simple/shopping/ShoppingApplication.java
@@ -0,0 +1,23 @@
+package com.simple.shopping;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@EnableWebMvc
+@SpringBootApplication
+public class ShoppingApplication implements WebMvcConfigurer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingApplication.class, args);
+	}
+
+}
