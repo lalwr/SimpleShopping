26cb211e5c85450a57bb3c18967d35cdb4857a77
lalwr <lalwrj@gmail.com>
2018-04-26 18:38:46 +0900

    feat : login udpate

--- src/main/java/com/simple/shopping/controller/UserController.java
+++ src/main/java/com/simple/shopping/controller/UserController.java
@@ -1,20 +1,11 @@
 package com.simple.shopping.controller;
 
-import com.simple.shopping.domain.User;
-import com.simple.shopping.domain.UserRole;
 import com.simple.shopping.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("/users")
@@ -22,33 +13,6 @@ public class UserController {
 
     @Autowired
     UserService userService;
-
-    @GetMapping(path = "/join")
-    public String userJoin(ModelMap modelMap){
-        User user = new User();
-        modelMap.addAttribute("user", user);
-        return "users/join";
-    }
-    @PostMapping(path = "/join")
-    public String join(@ModelAttribute User user){
-
-        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-
-        List<UserRole> userRoleList = new ArrayList<>();
-        UserRole userRole = new UserRole();
-        userRole.setRoleName("USER");
-        user.setRoles(userRoleList);
-
-        User saveUser = userService.addUser(user);
-
-        return "redirect:/";
-    }
-
-    @GetMapping(path = "/login")
-    public String login() { return "users/login"; }
-
-
 
     @GetMapping(path = "/user")
     public String user() { return "users/user"; }
