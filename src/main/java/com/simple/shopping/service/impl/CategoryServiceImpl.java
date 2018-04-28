655140cb4c3feb0fa1514a27842b989f5714b7a5
machme <luasnk@gmail.com>
2018-04-28 09:11:39 +0900

    feat: feed back applied

--- src/main/java/com/simple/shopping/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/simple/shopping/service/impl/CategoryServiceImpl.java
@@ -3,14 +3,18 @@ package com.simple.shopping.service.impl;
 import com.simple.shopping.domain.Category;
 import com.simple.shopping.repository.CategoryRepository;
 import com.simple.shopping.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
 
     CategoryRepository categoryRepository;
 
+    @Autowired
     public CategoryServiceImpl(CategoryRepository categoryRepository){
         this.categoryRepository = categoryRepository;
     }
@@ -20,5 +24,11 @@ public class CategoryServiceImpl implements CategoryService{
     public Category addCategory(Category ctgr) {
         Category savedCtgr = categoryRepository.save(ctgr);
         return savedCtgr;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Category> getCategories() {
+        return categoryRepository.findAll();
     }
 }
