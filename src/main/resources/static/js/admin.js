83bdf913039adc38893a871fa1daa5d52c2f0e77
machme <luasnk@gmail.com>
2018-05-06 15:20:23 +0900

    fix: feed back 반영

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -8,3 +8,42 @@ function submitByMethod(formId, method){
     form.appendChild(eMethod);
     form.submit();
 }
+
+function submitWithJson(method, checkName, attr, paramName, items){
+    var categoryInfo = new Object();
+    var categoryList = new Array();
+    var checks = document.getElementsByName(checkName);
+    var checkedOne = false;
+
+    for(var i=0, size = checks.length ; i < size ; i++){
+        var check = checks[i];
+
+        if(check.checked == true){
+            checkedOne = true;
+            var idx = check.getAttribute(attr);
+            var category = new Object();
+            category[checkName] = check.value;
+
+            for(var j=0, itemSize = items.length ; j < itemSize ; j++ ){
+                var item = document.getElementById(items[j]+'.'+idx);
+                category[items[j]] = item.value;
+            }
+            categoryList.push(category);
+        }
+    }
+    if(checkedOne==false){return;}
+    categoryInfo[paramName] = categoryList;
+
+
+    $.ajax({
+        type : method,
+        url : context+'admin/category/list',
+        contentType : 'application/json; charset=UTF-8',
+        data : JSON.stringify(categoryInfo),
+        dataType : 'text',
+        success: function(data){
+            document.location.reload();
+        }
+    });
+
+}
